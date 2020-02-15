35d8b6f7913203fec29607dafda0d0e57b179f7c
alex <alex@pdp7.net>
2020-02-15 17:53:57 +0100

    cargo fmt changes

--- src/bin/ragent.rs
+++ src/bin/ragent.rs
@@ -9,16 +9,12 @@ use ragent::get_ragent_info;
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = ([0, 0, 0, 0], 21488).into();
-    let new_service = make_service_fn(|_| {
-        async {
-            Ok::<_, Infallible>(service_fn(|_| {
-                async {
-                    Ok::<_, Infallible>(Response::new(Body::from(
-                        serde_json::to_string(&get_ragent_info()).unwrap(),
-                    )))
-                }
-            }))
-        }
+    let new_service = make_service_fn(|_| async {
+        Ok::<_, Infallible>(service_fn(|_| async {
+            Ok::<_, Infallible>(Response::new(Body::from(
+                serde_json::to_string(&get_ragent_info()).unwrap(),
+            )))
+        }))
     });
     let server = Server::bind(&addr).serve(new_service);
 
